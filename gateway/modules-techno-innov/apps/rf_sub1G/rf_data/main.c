0c27d35d9c0f627ec2579399c80ab6f8bef728c3
nicolas <nicolas.jannolfo@cpe.fr>
2019-11-04 14:52:11 +0100

    Modification sur rf_data pour test

--- gateway/modules-techno-innov/apps/rf_sub1G/rf_data/main.c
+++ gateway/modules-techno-innov/apps/rf_sub1G/rf_data/main.c
@@ -1,4 +1,3 @@
-
 /****************************************************************************
  *   apps/rf_sub1G/simple/main.c
  *
@@ -22,7 +21,6 @@
  *
  *************************************************************************** */
 
-
 #include "core/system.h"
 #include "core/systick.h"
 #include "core/pio.h"
@@ -30,32 +28,31 @@
 #include "drivers/serial.h"
 #include "drivers/gpio.h"
 #include "drivers/ssp.h"
-#include "drivers/i2c.h"
-#include "drivers/adc.h"
-#include "extdrv/ssd130x_oled_driver.h"
-#include "extdrv/ssd130x_oled_buffer.h"
 #include "extdrv/cc1101.h"
 #include "extdrv/status_led.h"
-#include "extdrv/tmp101_temp_sensor.h"
-#include "lib/font.h"
+#include "drivers/i2c.h"
 
+#include "extdrv/bme280_humidity_sensor.h"
+#include "extdrv/veml6070_uv_sensor.h"
+#include "extdrv/tsl256x_light_sensor.h"
 
-#define MODULE_VERSION	0x02
+#define MODULE_VERSION	0x03
 #define MODULE_NAME "RF Sub1G - USB"
 
-
-#define RF_868MHz  1 // Utilisée (1)
+#define RF_868MHz  1
 #define RF_915MHz  0
 #if ((RF_868MHz) + (RF_915MHz) != 1)
 #error Either RF_868MHz or RF_915MHz MUST be defined.
 #endif
 
-
 #define DEBUG 1
 #define BUFF_LEN 60
+#define RF_BUFF_LEN  64
 
 #define SELECTED_FREQ  FREQ_SEL_48MHz
-/************************************************DEBUG***************************** */
+#define DEVICE_ADDRESS  0x17 /* Addresses 0x00 and 0xFF are broadcast */
+#define NEIGHBOR_ADDRESS 0x12 /* Address of the associated device */
+/***************************************************************************** */
 /* Pins configuration */
 /* pins blocks are passed to set_pins() for pins configuration.
  * Unused pin blocks can be removed safely with the corresponding set_pins() call
@@ -65,19 +62,13 @@ const struct pio_config common_pins[] = {
 	/* UART 0 */
 	{ LPC_UART0_RX_PIO_0_1,  LPC_IO_DIGITAL },
 	{ LPC_UART0_TX_PIO_0_2,  LPC_IO_DIGITAL },
-	/* I2C 0 */
-	{ LPC_I2C0_SCL_PIO_0_10, (LPC_IO_DIGITAL | LPC_IO_OPEN_DRAIN_ENABLE) },
-	{ LPC_I2C0_SDA_PIO_0_11, (LPC_IO_DIGITAL | LPC_IO_OPEN_DRAIN_ENABLE) },
 	/* SPI */
 	{ LPC_SSP0_SCLK_PIO_0_14, LPC_IO_DIGITAL },
 	{ LPC_SSP0_MOSI_PIO_0_17, LPC_IO_DIGITAL },
 	{ LPC_SSP0_MISO_PIO_0_16, LPC_IO_DIGITAL },
-	/* ADC */
-	{ LPC_ADC_AD0_PIO_0_30, LPC_IO_ANALOG },
-	{ LPC_ADC_AD1_PIO_0_31, LPC_IO_ANALOG },
-	{ LPC_ADC_AD2_PIO_1_0,  LPC_IO_ANALOG },
-	/* RESET PIN */
-	{ LPC_GPIO_0_0, (LPC_IO_MODE_PULL_UP | LPC_IO_DIGITAL) },
+	/* I2C 0 */
+	{ LPC_I2C0_SCL_PIO_0_10, (LPC_IO_DIGITAL | LPC_IO_OPEN_DRAIN_ENABLE) },
+	{ LPC_I2C0_SDA_PIO_0_11, (LPC_IO_DIGITAL | LPC_IO_OPEN_DRAIN_ENABLE) },
 	ARRAY_LAST_PIO,
 };
 
@@ -86,33 +77,20 @@ const struct pio cc1101_miso_pin = LPC_SSP0_MISO_PIO_0_16;
 const struct pio cc1101_gdo0 = LPC_GPIO_0_6;
 const struct pio cc1101_gdo2 = LPC_GPIO_0_7;
 
-typedef struct msg_data {
-	float temp;
-	float hum;
-	float lum;
-	char msg[50];
-} msg_data;
-
-typedef struct msg {
-	int id_mc;
-	msg_data data;
-} msg;
-
-#define TMP101_ADDR  0x94  /* Pin Addr0 (pin5 of tmp101) connected to VCC */
-struct tmp101_sensor_config tmp101_sensor = {
-	.bus_num = I2C0,
-	.addr = TMP101_ADDR,
-	.resolution = TMP_RES_ELEVEN_BITS,
-};
-const struct pio temp_alert = LPC_GPIO_0_3;
-
 const struct pio status_led_green = LPC_GPIO_0_28;
 const struct pio status_led_red = LPC_GPIO_0_29;
 
+const struct pio button = LPC_GPIO_0_12; /* ISP button */
 
-#define ADC_VBAT  LPC_ADC(0)
-#define ADC_EXT1  LPC_ADC(1)DEBUG
-#define ADC_EXT2  LPC_ADC(2)
+// Message
+struct message 
+{
+	uint32_t temp;
+	uint16_t hum;
+	uint32_t lum;
+};
+typedef struct message message;
+
 
 /***************************************************************************** */
 void system_init()
@@ -140,13 +118,6 @@ void fault_info(const char* name, uint32_t len)
 	while (1);
 }
 
-
-
-
-/******************************************************************************/
-/* RF Communication */
-#define RF_BUFF_LEN  64
-
 static volatile int check_rx = 0;
 void rf_rx_calback(uint32_t gpio)
 {
@@ -172,12 +143,14 @@ void rf_config(void)
 	/* And change application specific settings */
 	cc1101_update_config(rf_specific_settings, sizeof(rf_specific_settings));
 	set_gpio_callback(rf_rx_calback, &cc1101_gdo0, EDGE_RISING);
-
+    cc1101_set_address(DEVICE_ADDRESS);
 #ifdef DEBUG
 	uprintf(UART0, "CC1101 RF link init done.\n\r");
 #endif
 }
 
+
+uint8_t chenillard_active = 1;
 void handle_rf_rx_data(void)
 {
 	uint8_t data[RF_BUFF_LEN];
@@ -189,42 +162,79 @@ void handle_rf_rx_data(void)
 	/* Go back to RX mode */
 	cc1101_enter_rx_mode();
 
+	uprintf(UART0, "RF: ret:%d, st: %d.\n\r", ret, status);
+    uprintf(UART0, "RF: data lenght: %d.\n\r", data[0]);
+    uprintf(UART0, "RF: destination: %x.\n\r", data[1]);
+    uprintf(UART0, "RF: message in memory is not visible: %c.\n\r", data[2]);
+
 #ifdef DEBUG
 	uprintf(UART0, "RF: ret:%d, st: %d.\n\r", ret, status);
+    uprintf(UART0, "RF: data lenght: %d.\n\r", data[0]);
+    uprintf(UART0, "RF: destination: %x.\n\r", data[1]);
+    uprintf(UART0, "RF: message in memory is not visible: %c.\n\r", data[2]);
 #endif
 
-			uprintf(UART0, "Recu : %s", data);
+	switch (data[2]) {
+		case '0':
+			{
+				chenillard_active = 0;
+			}
+			break;
+		case '1':
+			{
+				chenillard_active = 1 ;
+			}
+			break;
+	}
 }
 
-
-/* Data sent on radio comes from the UART, put any data received from UART in
- * cc_tx_buff and send when either '\r' or '\n' is received.
- * This function is very simple and data received between cc_tx flag set and
- * cc_ptr rewind to 0 may be lost. */
 static volatile uint32_t cc_tx = 0;
 static volatile uint8_t cc_tx_buff[RF_BUFF_LEN];
 static volatile uint8_t cc_ptr = 0;
-void handle_uart_cmd(uint8_t c)
-{
-#ifdef DEBUG
-	uprintf(UART0, "Received command : %c, buffer size: %d.\n\r",c,cc_ptr);
-#endif
-	if (cc_ptr < RF_BUFF_LEN) {
-		cc_tx_buff[cc_ptr++] = c;
-	} else {
-		cc_ptr = 0;
-	}
-	if ((c == '\n') || (c == '\r')) {
-		cc_tx = 1;
-	}
+uint8_t chenillard_activation_request = 1;
+void activate_chenillard(uint32_t gpio) {
+	if (chenillard_activation_request == 1){
+        cc_tx_buff[0]='0';
+        cc_ptr = 1;
+        cc_tx=1;
+        chenillard_activation_request = 0;
+    }
+    else{
+        cc_tx_buff[0]='1';
+        cc_ptr = 1;
+        cc_tx=1;
+        chenillard_activation_request = 1;
+    }
 }
 
-static volatile msg cc_tx_msg;
+
+/***************************************************************************** */
+/* Luminosity */
+
+/* Note : These are 8bits address */
+#define TSL256x_ADDR   0x52 /* Pin Addr Sel (pin2 of tsl256x) connected to GND */
+struct tsl256x_sensor_config tsl256x_sensor = {
+	.bus_num = I2C0,
+	.addr = TSL256x_ADDR,
+	.gain = TSL256x_LOW_GAIN,
+	.integration_time = TSL256x_INTEGRATION_100ms,
+	.package = TSL256x_PACKAGE_T,
+}; 
+
+static volatile uint32_t update_display = 0;
+
+/***************************************************************************** */
+void periodic_display(uint32_t tick)
+{
+	update_display = 1;
+}
+
+static volatile message cc_tx_msg;
 void send_on_rf(void)
 {
-	msg_data data;
-	uint8_t cc_tx_data[sizeof(msg)+2];
-	cc_tx_data[0]=sizeof(msg)+1;
+	message data;
+	uint8_t cc_tx_data[sizeof(message)+2];
+	cc_tx_data[0]=sizeof(message)+1;
 	cc_tx_data[1]=NEIGHBOR_ADDRESS;
 	data.hum=cc_tx_msg.hum;
 	data.lum=cc_tx_msg.lum;
@@ -239,49 +249,67 @@ void send_on_rf(void)
 	int ret = cc1101_send_packet(cc_tx_data, sizeof(message)+2);
 
 #ifdef DEBUG
-	uprintf(UART0, "Tx ret: %d\n\r", ret);
-    uprintf(UART0, "RF: data lenght: %d.\n\r", cc_tx_data[0]);
-    uprintf(UART0, "RF: destination: %x.\n\r", cc_tx_data[1]);
-    uprintf(UART0, "RF: message: %c.\n\r", cc_tx_data[2]);
+	//uprintf(UART0, "Tx ret: %d\n\r", ret);
+    //uprintf(UART0, "RF: data lenght: %d.\n\r", cc_tx_data[0]);
+    //uprintf(UART0, "RF: destination: %x.\n\r", cc_tx_data[1]);
+    //uprintf(UART0, "RF: message: %c.\n\r", cc_tx_data[2]);
 #endif
 }
 
-
-/***************************************************************************** */
 int main(void)
 {
-	char data[20];
-	int ret = 0;
-
 	system_init();
-	uart_on(UART0, 115200, handle_uart_cmd);
-	ssp_master_on(0, LPC_SSP_FRAME_SPI, 8, 4*1000*1000); /* bus_num, frame_type, data_width, rate */
+	uart_on(UART0, 115200, NULL);
 	i2c_on(I2C0, I2C_CLK_100KHz, I2C_MASTER);
-	adc_on(NULL);
+	ssp_master_on(0, LPC_SSP_FRAME_SPI, 8, 4*1000*1000); /* bus_num, frame_type, data_width, rate */
+	status_led_config(&status_led_green, &status_led_red);
 
 	/* Radio */
 	rf_config();
 
-	char received_data[30];
+	add_systick_callback(periodic_display, 1000);
 
-	//-------------------------------------------------	
+    /* Activate the chenillard on Rising edge (button release) */
+	set_gpio_callback(activate_chenillard, &button, EDGE_RISING);
+
+	uprintf(UART0, "App started\n\r");
 
 	while (1) {
 		uint8_t status = 0;
 
-		/* Tell we are alive :) */
-		chenillard(250);
+		
+        /* Verify that chenillard is enable */
+        if (chenillard_active == 1) {
+			/* Tell we are alive :) */
+		    chenillard(250);
+        }
+        else{
+            status_led(none);
+			msleep(250);
+        }
 
-		/* RF */
-		cc_tx = 1;
-		if (cc_tx == 1) {
-			#ifdef DEBUG
-			uprintf(UART0, "Transmission ready to send.\n\r");
-			#endif
+		if (update_display == 1) {
+			uint16_t uv = 0, ir = 0, humidity = 0;
+			uint32_t pressure = 0, temp = 0, lux = 0;
+			
+			/* Read the sensors */
+			/*
+			uv_display(UART0, &uv);
+			bme_display(UART0, &pressure, &temp, &humidity);
+			lux_display(UART0, &ir, &lux);
+			cc_tx_msg.temp=temp;
+			cc_tx_msg.hum=humidity;
+			cc_tx_msg.lum=lux;
+			update_display = 0;
+			*/
+
 			send_on_rf();
-			cc_tx = 0;
 		}
 
+		/* RF */
+		if (cc_tx == 1) {
+			cc_tx = 0;
+		}
 		/* Do not leave radio in an unknown or unwated state */
 		do {
 			status = (cc1101_read_status() & CC1101_STATE_MASK);
@@ -298,15 +326,12 @@ int main(void)
 				loop = 0;
 			}
 		}
-
 		if (check_rx == 1) {
-			#ifdef DEBUG
-			uprintf(UART0, "We are ready to receive.\n\r");
-			#endif
 			check_rx = 0;
 			handle_rf_rx_data();
 		}
 
+		
 	}
 	return 0;
 }
